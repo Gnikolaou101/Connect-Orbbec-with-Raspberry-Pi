1b94a3ccdaae78eb82acccbf87e20606c070da74
ΑΓΩ Simulations <agwsimulations@gmail.com>
2019-02-20 16:23:54 +0200

    test the python code if it works correct on  localhost

---
+++ onLocalhost/server.py
@@ -0,0 +1,23 @@
+
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import socket
+#The first line it is needed for the orbbec to understand the coding
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print 'Connection address:', addr
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: break
+    print "received data:", data
+    conn.send(data)  # echo
+conn.close()
